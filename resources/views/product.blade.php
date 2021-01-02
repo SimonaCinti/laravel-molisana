2f22407008668c326f8d2b45a2b77d2e9a6a421e
Simona Cinti <simona.cinti2@gmail.com>
2021-01-02 16:25:26 +0100

    product arrow loop

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -6,11 +6,15 @@
     <div class="navigation">
         @if($id > 0)
             <a href="{{route('product', $id - 1)}}">&lt; PREV</a>
+        @elseif($id == 0)
+            <a href="{{route('product', $length)}}">&lt; PREV</a>
         @endif
     </div>
     <div class="navigation">
         @if($id < $length )
             <a href="{{route('product', $id + 1)}}">NEXT &gt;</a>
+        @elseif($id = $length)
+            <a href="{{route('product', 0)}}">NEXT &gt; </a>
         @endif
     </div>
     <div class="container">
